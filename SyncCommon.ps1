f085fd5e79b6cddbc8a7835805cbb8fe6ee1f9a9
Jason Vercellone <jason.vercellone@gmail.com>
2017-08-30 22:04:25 -0500

    Appveyor
    Includes initial deployment of Fix #2

---
+++ SyncCommon.ps1
@@ -0,0 +1,4 @@
+﻿$Path = Split-Path -Parent $MyInvocation.MyCommand.Path
+Get-ChildItem -Path $Path -Directory -Exclude Common | ForEach-Object {
+    Copy-Item -Path (Join-Path $Path 'Common\F5-LTM') -Destination $_ -Force -Recurse
+}
